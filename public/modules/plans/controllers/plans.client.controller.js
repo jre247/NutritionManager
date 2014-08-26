40ebe14c289f8333114880cabf4478e26af5e2a5
jason <jevans8@gmail.com>
2014-08-26 11:59:02 -0400

    copy plan no longer mistakenly calls to server

--- public/modules/plans/controllers/plans.client.controller.js
+++ public/modules/plans/controllers/plans.client.controller.js
@@ -402,42 +402,6 @@ angular.module('plans').controller('PlansController', ['$scope', '$stateParams',
                 $log.info('Modal dismissed at: ' + new Date());
             });
         };
-
-
-        //accordian code
-//        $scope.groups = [
-//            {
-//                title: 'Dynamic Group Header - 1',
-//                content: 'Dynamic Group Body - 1'
-//            },
-//            {
-//                title: 'Dynamic Group Header - 2',
-//                content: 'Dynamic Group Body - 2'
-//            }
-//        ];
-//
-//        $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-//        $scope.addItem = function() {
-//            var newItemNo = $scope.items.length + 1;
-//            $scope.items.push('Item ' + newItemNo);
-//        };
-//        $scope.status = {
-//            isFirstOpen: true,
-//            isFirstDisabled: false
-//        };
-
-
-
-        // Use the old watch() with default object equality,
-        // which defaults to use object REFERENCE checks.
-//        $scope.$watch(
-//            "collection",
-//            function( newValue, oldValue ) {
-//
-//                addLogItem( $scope.watchLog );
-//
-//            }
-//        );
 	}
 ]);
 
